1720611d435fc6ceb98a901315c9efb61cec38f7
Zombieteer <aryan.nigam1996@gmail.com>
2020-02-21 00:38:46 +0530

    Styling todo

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,46 +1,91 @@
-import React, {useState} from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity} from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 
-const Todo=()=> {
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-  const [todos, setTodos] = useState([])
-  const [newTodo, setNewTodo] = useState('')
-
-  const handleChange = (text)=> {
-    setNewTodo(text)
-  }
+  const handleChange = text => {
+    setNewTodo(text);
+  };
 
   const handlePress = () => {
-    setTodos([...todos, newTodo])
-    setNewTodo('')
-  }
+    if (newTodo!==''){
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <TextInput value={newTodo} onChangeText={handleChange} style={styles.input}/>
-      <TouchableOpacity onPress={handlePress}>
-        <Text>create</Text>
-      </TouchableOpacity>
-      <View style={{marginTop: '3%'}}>
-        {todos.map((todo, i) => <Text key={i}>{todo}</Text>)} 
+    <View 
+    style={styles.container}
+    >  
+      <View style={styles.form}>
+        <TextInput
+          value={newTodo}
+          onChangeText={handleChange}
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>create</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={ styles.todos }>
+        {todos.map((todo, i) => (
+          <View style={styles.todo} key={i}>
+            <Text  style={styles.todoText} >{todo}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    // justifyContent: "center"
+    padding: 20,
+    marginTop: 20
   },
   input: {
     backgroundColor: "#f2f1f1",
-    width: '80%',
-    marginTop: '10%'
+    width: '70%',
+    fontSize: 20,
+  },
+  form : {
+    flexDirection: 'row'
+  },
+  button : {
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '25%',
+    marginLeft: '5%',
+    height: 50
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  todos: {
+    marginTop: 40
+  },
+  todo: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+    marginBottom: 10
+  },
+  todoText: {
+    fontSize: 20
   }
 });
 
-export default Todo
+export default Todo;
